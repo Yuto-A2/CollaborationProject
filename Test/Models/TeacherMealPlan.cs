01cf3a38c8f486e4aef18ae23755845c5efe43e9
soysauce4444 <105949254+soysauce4444@users.noreply.github.com>
2024-08-02 13:52:16 -0400

    Added documentation explaining relationships betwen student/teacher entities and student/teachermealplan tables.

--- Test/Models/TeacherMealPlan.cs
+++ Test/Models/TeacherMealPlan.cs
@@ -7,13 +7,20 @@ using System.Web;
 
 namespace Test.Models
 {
+    // a table linking teachers to their individual meal plans, and the meal plan table
     public class TeacherMealPlan
     {
         [Key]
         public int teacher_meal_plan_id { get; set; }
+
+        // each individual teacher meal plan applies to one teacher
+        // this table has the foreign key because teachermealplan depends on the teacher
         [ForeignKey("Teacher")]
         public int teacher_id { get; set; }
         public virtual Teacher Teacher { get; set; }
+
+        // an individual teacher meal plan can only belong to one meal plan tier
+        // each meal plan tier can apply to many individual teacher meal plans
         [ForeignKey("MealPlan")]
         public int plan_id { get; set; }
         public virtual MealPlan MealPlan { get; set; }
