570a93e8b195ebc40e828a03d461b5295c6ad1e2
Yuto Arimori <n01648493@humber.ca>
2024-08-09 14:19:40 -0400

    Updated the database of StudentMealPlan to upload pictures.

---
+++ Test/Migrations/202408091803526_StudentMealPic.cs
@@ -0,0 +1,20 @@
+﻿namespace Test.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class StudentMealPic : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.StudentMealPlans", "StudentMealPlanHasPic", c => c.Boolean(nullable: false));
+            AddColumn("dbo.StudentMealPlans", "PicExtension", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.StudentMealPlans", "PicExtension");
+            DropColumn("dbo.StudentMealPlans", "StudentMealPlanHasPic");
+        }
+    }
+}
